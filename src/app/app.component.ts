674e403b78d5a241351d4a92ea2606265d15f022
Tarek chafroud <chafroudtarek6@gmail.com>
2022-07-02 13:42:58 +0100

    navabar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faBars,faTimes } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  faBars = faBars;
+  faTimes = faTimes;
+  openMenu:any ;
   title = 'all-we-do';
+
+  showMenu(){
+    this.openMenu = 0;
+  }
+
+  hideMenu(){
+    this.openMenu = -200;
+  }
 }
